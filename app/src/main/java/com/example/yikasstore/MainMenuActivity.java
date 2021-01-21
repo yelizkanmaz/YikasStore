697cd1ceafe1e7395293b1d701e8307132024e60
yelizkanmaz <yelizkanmaz@hotmail.com>
2021-01-21 23:23:22 +0300

    yikas-24 - Menü eklenecek

--- app/src/main/java/com/example/yikasstore/MainMenuActivity.java
+++ app/src/main/java/com/example/yikasstore/MainMenuActivity.java
@@ -1,17 +1,26 @@
 package com.example.yikasstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenuActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         getSupportActionBar().hide();
+
     }
+
 }
