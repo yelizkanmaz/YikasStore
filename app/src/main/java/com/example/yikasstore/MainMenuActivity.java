3ddfba02d20a9279ffae1f8b03b831324bccbf23
yelizkanmaz <yelizkanmaz@hotmail.com>
2021-01-24 21:21:12 +0300

    yikas-42 - Anasayfadan kategorilere geçişler kodlanacak

--- app/src/main/java/com/example/yikasstore/MainMenuActivity.java
+++ app/src/main/java/com/example/yikasstore/MainMenuActivity.java
@@ -1,26 +1,59 @@
 package com.example.yikasstore;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 public class MainMenuActivity extends AppCompatActivity {
-
+    ImageView imgCats;
+    ImageView imgDogs;
+    ImageView imgTags;
+    ImageView imgCollars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         getSupportActionBar().hide();
+        init();
 
+    }
+
+    private void init() {
+        imgCats = findViewById(R.id.imgCats);
+        imgDogs = findViewById(R.id.imgDogs);
+        imgTags = findViewById(R.id.imgTags);
+        imgCollars = findViewById(R.id.imgCollars);
+
+        imgCats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, CatsCategories.class));
+            }
+        });
+        imgDogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, DogsCategories.class));
+            }
+        });
+
+        imgTags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, TagsActivity.class));
+            }
+        });
+
+        imgCollars.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, CollarsActivity.class));
+            }
+        });
     }
 
 }
